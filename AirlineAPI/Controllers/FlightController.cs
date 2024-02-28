ca94df53c4b695e87f0489ba41687dabd91bbea6
Drevian1 <drtaylor@student.neumont.edu>
2024-02-27 20:07:32 -0700

    New Stuff

--- AirlineAPI/Controllers/FlightController.cs
+++ AirlineAPI/Controllers/FlightController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AirlineAPI.Models;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AirlineAPI.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using AirlineAPI.Data;
 
 namespace AirlineAPI.Controllers
 {
     public class FlightController : Controller
     {
-         
+        IDataAccessLayer dal;
+
+        private ApplicationDbContext db;
+
+        public FlightController(IDataAccessLayer indal)
+        {
+            dal = indal;
+        }
         public IActionResult Index()
         {
             return View();
@@ -13,19 +25,59 @@ namespace AirlineAPI.Controllers
 
         public IActionResult AddFlight()
         {
-
+            List<Flight> lstFlight = dal.Airlines();
+            ViewBag.Flight = new SelectList(lstFlight, "Id", "Title");
             return View();
+
+            
         }
 
-        public IActionResult CancelFlight()
+        public IActionResult GetFlight(int? Id)
         {
-            return View();
+            
+                if (Id == null)
+                {
+                    return BadRequest(); 
+                }
+
+                
+                Flight foundFlight = db.Airlines.FirstOrDefault(f => f.Id == Id);
+
+                if (foundFlight == null)
+                {
+                    return NotFound(); 
+                }
+
+                return View(foundFlight); 
+            
         }
 
-        public IActionResult SearchFlight() 
+        public IActionResult GetReservedFlight(int? id, int? FlightNumber)
         {
-            return View();
+            
+            return View(Flight);
         }
+
+        public IActionResult SearchFlight(int FlightNumber)
+        {
+
+            if (string.IsNullOrEmpty(Airline))
+            {
+                return View("ReservedFlight", dal.FlightNumber);
+            }
+            return View("ReservedFlight", dal.GetReservedFlight().Where(x => x.FlightNumber));
+        }
+        public IActionResult CancelFlight(int? Id)
+        {
+            dal.RemoveMovie(Id);
+            TempData["success"] = "Flight removed!";
+            return RedirectToAction("ReservedFlight", "Flight");
+
+        }
+
+        
+            
+        
 
         public IActionResult deleteFlight() 
         {
