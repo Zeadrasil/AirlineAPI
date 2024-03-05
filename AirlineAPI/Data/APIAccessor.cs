52785ea474e36b392a2420328f233b9a4b112c6a
LAUREN UNRUH <lunruh946@gmail.com>
2024-03-04 19:17:06 -0700

    apiacecessor stuff

--- AirlineAPI/Data/APIAccessor.cs
+++ AirlineAPI/Data/APIAccessor.cs
@@ -11,44 +11,52 @@ namespace AirlineAPI.Data
 		{
 			"example"
 		};
-		public static async Task<List<Flight>?> getFlights(DateOnly leaveAfter, DateOnly leaveBefore, 
-			string departureIATA, string arrivalIATA, DateOnly? arriveAfter = null, DateOnly? arriveBefore = null,
-			string? airlineIATA = null, int flightNumber = 0, string? aircraftIATA = null)
-		{
-			List<Flight> flights = new List<Flight>();
-			do
-			{
-				do
-				{
-					int attempts = 0;
-					HttpResponseMessage response = null;
-					do
-					{
-						string address = "https://api.aviationstack.com/v1/flights?access_key=" + getKey();
-						address += "&dep_iata=" + departureIATA;
-						address += "&arr_iata=" + arrivalIATA;
-						address += !string.IsNullOrEmpty(airlineIATA) ? "&airline_iata=" + airlineIATA : "";
-						address += flightNumber > 0 ? "&flight_number=" + flightNumber : "";
-						address += arriveAfter != null ? "&arr_scheduled_time_arr=" + arriveAfter?.ToString("yyyy-MM-dd") : "";
-						address += "&arr_scheduled_time_dep=" + leaveAfter.ToString("yyyy-MM-dd");
-						response = await client.GetAsync(address);
-						attempts++;
-					}
-					while (!response.IsSuccessStatusCode && attempts < 50);
-					if (response.IsSuccessStatusCode)
-					{
-						string result = await response.Content.ReadAsStringAsync();
-						flights.AddRange(parseFlight(result));
-					}
-					arriveAfter?.AddDays(1);
-				}
-				while (arriveAfter != null && arriveBefore != null && arriveAfter < arriveBefore);
-				leaveAfter.AddDays(1);
-			}
-			while (leaveAfter < leaveBefore);
-			return flights;
-		}
-		private static string getKey()
+        public static async Task<List<Flight>?> getFlights(DateOnly leaveAfter, DateOnly leaveBefore,
+            string departureIATA, string arrivalIATA, DateOnly? arriveAfter = null, DateOnly? arriveBefore = null,
+            string? airlineIATA = null, int flightNumber = 0, string? aircraftIATA = null)
+        {
+            List<Flight> flights = new List<Flight>();
+            do
+            {
+                do
+                {
+                    int attempts = 0;
+                    HttpResponseMessage response = null;
+                    do
+                    {
+                        string address = "https://api.aviationstack.com/v1/flights?access_key=" + getKey();
+                        address += "&dep_iata=" + departureIATA;
+                        address += "&arr_iata=" + arrivalIATA;
+                        address += !string.IsNullOrEmpty(airlineIATA) ? "&airline_iata=" + airlineIATA : "";
+                        address += flightNumber > 0 ? "&flight_number=" + flightNumber : "";
+                        address += arriveAfter != null ? "&arr_scheduled_time_arr=" + arriveAfter?.ToString("yyyy-MM-dd") : "";
+                        address += "&arr_scheduled_time_dep=" + leaveAfter.ToString("yyyy-MM-dd");
+                        response = await client.GetAsync(address);
+                        attempts++;
+                    }
+                    while (!response.IsSuccessStatusCode && attempts < 50);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        flights.AddRange(parseFlight(result));
+
+                        // Parse aircraft types
+                        List<AircraftType> aircraftTypes = parseAircraftType(result);
+                        
+                    }
+                    arriveAfter?.AddDays(1);
+                }
+                while (arriveAfter != null && arriveBefore != null && arriveAfter < arriveBefore);
+                leaveAfter.AddDays(1);
+            }
+            while (leaveAfter < leaveBefore);
+            return flights;
+        }
+
+
+
+
+        private static string getKey()
 		{
 			return apiKeys[new Random().Next(apiKeys.Length)];
 		}
@@ -172,7 +180,9 @@ namespace AirlineAPI.Data
 			return aircraftTypes;
         }
 
-        private static DateTime getDateTimeFromString(string str)
+
+
+    private static DateTime getDateTimeFromString(string str)
 		{
 			DateTime dt = new DateTime();
 			if (int.Parse(str.Substring(0, 4)) > 0)
